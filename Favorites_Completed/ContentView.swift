74ff730b8f574c48866444c773a0a4b46d6f5409
Abhishek Jariwala <119805313+ajariwala1@users.noreply.github.com>
2025-07-04 18:42:22 -0700

    Initial Commit

---
+++ Favorites_Completed/ContentView.swift
@@ -0,0 +1,24 @@
+//
+// ContentView.swift : Favorites_Completed
+//
+// Copyright © 2025 Auburn University.
+// All Rights Reserved.
+
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Favorites_Completed/Favorites_CompletedApp.swift
@@ -0,0 +1,17 @@
+//
+// Favorites_CompletedApp.swift : Favorites_Completed
+//
+// Copyright © 2025 Auburn University.
+// All Rights Reserved.
+
+
+import SwiftUI
+
+@main
+struct Favorites_CompletedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
