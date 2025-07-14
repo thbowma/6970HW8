2078fac2c5de36511f8eafcf6244ec9cfa93430f
Thomas Bowman <thomas@MacBookPro.lan>
2025-07-14 01:54:05 -0500

    Initial

--- Favorites_Completed/Views/FavoritesView.swift
+++ Favorites_Completed/Views/FavoritesView.swift
@@ -8,11 +8,47 @@
 import SwiftUI
 
 struct FavoritesView: View {
+    @EnvironmentObject var favorites: FavoritesViewModel
+    
     var body: some View {
-        Text("Favorites")
+        ScrollView {
+            LazyVStack {
+                ForEach(favorites.cities.indices, id: \.self) {index in
+                    if favorites.cities[index].isFavorite {
+                        CityCardView(city: favorites.cities[index])
+                    }
+                }
+                
+                
+                ForEach(favorites.hobbies.indices, id: \.self) {index in
+                    if favorites.hobbies[index].isFavorite {
+                        HobbyRowView(hobby: favorites.hobbies[index])
+                    }
+                }
+                
+             
+                
+                ForEach(favorites.books.indices, id: \.self) {index in
+                    if favorites.books[index].isFavorite {
+                        BookRowView(book: favorites.books[index])
+                    }
+                }
+
+
+
+            }
+        }
     }
 }
 
 #Preview {
     FavoritesView()
 }
+
+
+//for index in items.indices {
+//    items[index].isFavorite = false
+//}
+//ForEach(favorites.filteredCities(searchText: searchText)) { city in
+//    CityCardView(city: city)
+//}
